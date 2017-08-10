82a21c4b1a9f9922fa0736f77fafebb0a1aa4633
Rnso <rnso.pnr@gmail.com>
2017-08-10 15:13:06 +0400

    Git commit

--- config.js
+++ config.js
@@ -2,14 +2,12 @@ const env = process.env
 
 export const nodeEnv = env.NODE_ENV || 'development'
 
-export const API_KEY_PLACE = 'AIzaSyBdMpshfj7xG_fcr6l3ph19KiHGE1_LVT0'
-export const API_KEY_GEOCODE = 'AIzaSyDj4oDbphHS81GkioQqzVC43Et039KvFGQ'
-export const CLIENT_ID = '687248084198-an500k40vequ770s5tccbtaa1nsmgnbo.apps.googleusercontent.com'
+export const API_KEY_PLACE = 'PUT YOUR API_KEY'
+export const CLIENT_ID = 'PUT YOU CLIENT_ID'
 
 
 export default{
-    mongodbUri: 'mongodb://rnso:pongen@ds159892.mlab.com:59892/fcc',
-    //mongodbUri: 'mongodb://localhost:27017/nightLife',
+    mongodbUri: 'mongodb://localhost:27017/nightLife',
     port: env.PORT || 9000,
     host: env.HOST || '0.0.0.0'
 }
