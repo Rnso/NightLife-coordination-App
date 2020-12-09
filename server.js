b1897af1b6419117572a9d2751c4efbd9d998b8c
rnso <rnso.pnr@gmail.com>
2020-12-09 16:22:32 +0400

    change start scripts in package.json

--- server.js
+++ server.js
@@ -3,17 +3,8 @@ import express from 'express'
 import router from './api'
 import path from 'path'
 import bodyParser from 'body-parser'
-/*import cors from 'cors'
 
 const server = express()
-
-server.use(cors({
-    origin: [
-        'http://localhost:8080'
-    ],
-    credentials: true 
-}))*/
-
 server.use(bodyParser.json() )
 server.use('/api', router)
 server.use(express.static('dist'))
