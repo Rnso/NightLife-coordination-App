f5c8c9a969b9c1c05d397d63ce14ffa1a079f95f
rnso <rnso.pnr@gmail.com>
2020-12-09 16:49:08 +0400

    changes for github repo

--- constant.js
+++ constant.js
@@ -1,6 +1,6 @@
-export const serverUrl = 'https://nightlife-cordination.herokuapp.com'
+//export const serverUrl = 'https://nightlife-cordination.herokuapp.com'
 
-//export const serverUrl = 'http://localhost:9000'
+export const serverUrl = 'http://localhost:9000'
 
-export const API_KEY_CODE = 'AIzaSyBdMpshfj7xG_fcr6l3ph19KiHGE1_LVT0'
+export const API_KEY_CODE = 'XXXX'
 
