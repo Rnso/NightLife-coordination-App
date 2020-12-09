73fdb49b2e4d18805d16e25b2a24057bff9e8c69
rnso <rnso.pnr@gmail.com>
2020-12-09 16:28:03 +0400

    update server url

--- constant.js
+++ constant.js
@@ -1,5 +1,6 @@
-//export const serverUrl = 'https://nightlife-cordination.herokuapp.com'
-export const serverUrl = 'http://localhost:9000'
+export const serverUrl = 'https://nightlife-cordination.herokuapp.com'
+
+//export const serverUrl = 'http://localhost:9000'
 
 export const API_KEY_CODE = 'AIzaSyBdMpshfj7xG_fcr6l3ph19KiHGE1_LVT0'
 
